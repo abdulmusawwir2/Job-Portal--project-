afdabbd31f61fa606d2942e625a22e8b2a7f711e
Abdul Musawwir <abdulmusawwir611@gmail.com>
2025-03-17 21:49:48 +0530

    commit vercel.json file

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.post('/webhooks',clerkWebhooks)
 
 
 //Port
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT, () => {
